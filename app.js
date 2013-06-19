5e9413052f190305848deedcf962cd898a3d0ed0
Kana Natsuno <dev@whileimautomaton.net>
2013-06-19 20:08:19 +0900

    Define listNaturalNumbers

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 (function () {
   var N = 10;
+
+  function listNaturalNumbers(n) {
+    var ns = [];
+    for (var i = 1; i <= n; i++)
+      ns.push(i);
+    return ns;
+  }
 })();
 // vim: expandtab softtabstop=2 shiftwidth=2
