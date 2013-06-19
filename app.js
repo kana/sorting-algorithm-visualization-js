4011dde522449b5b326e9a3e20fe80624dd150aa
Kana Natsuno <dev@whileimautomaton.net>
2013-06-19 20:08:31 +0900

    Define shuffle

--- app.js
+++ app.js
@@ -11,5 +11,16 @@
   function random(a, b) {
     return Math.floor(Math.random() * (b - a)) + a;
   }
+
+  function shuffle(ns) {
+    var n = ns.length;
+    for (var i = 0; i < n; i++) {
+      var j = random(0, n);
+      var t = ns[i];
+      ns[i] = ns[j];
+      ns[j] = t;
+    }
+    return ns;
+  }
 })();
 // vim: expandtab softtabstop=2 shiftwidth=2
